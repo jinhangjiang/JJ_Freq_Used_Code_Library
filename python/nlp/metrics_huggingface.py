b3f58266b93e4f7130a5df4071ec7969be217e7c
Jinhang Jiang <60441304+jinhangjiang@users.noreply.github.com>
2022-06-04 10:50:28 -0500

    Update metrics_huggingface.py

--- python/nlp/metrics_huggingface.py
+++ python/nlp/metrics_huggingface.py
@@ -1,19 +1,19 @@
 # smape
 def compute_metrics(eval_pred):
 
-  predictions, labels = eval_pred
+  predictions, references = eval_pred
   predictions = np.argmax(predictions, axis=1)
 
   # SMAPE
-  smape = 1/len(labels) * np.sum(2 * np.abs(predictions-labels) / (np.abs(labels) + np.abs(predictions))*100)
+  smape = 1/len(references) * np.sum(2 * np.abs(predictions-references) / (np.abs(references) + np.abs(predictions))*100)
  
   return {"smape": smape} 
 
 # f1
 def compute_metrics(eval_pred):
 
-  predictions, labels = eval_pred
+  predictions, references = eval_pred
   predictions = np.argmax(predictions, axis=1)
 
   # 'micro', 'macro', etc. are for multi-label classification. If you are running a binary classification, leave it as default or specify "binary" for average
-  return metric.compute(predictions=predictions, references=labels, average="micro")  
+  return metric.compute(predictions=predictions, references=references, average="micro")  

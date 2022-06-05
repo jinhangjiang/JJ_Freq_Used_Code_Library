e26ea49e59a0a48711b829e44d6172c96e449b6c
Jinhang Jiang <60441304+jinhangjiang@users.noreply.github.com>
2022-06-05 10:13:42 -0500

    Update metrics_huggingface.py

--- python/nlp/metrics_huggingface.py
+++ python/nlp/metrics_huggingface.py
@@ -1,19 +1,8 @@
-# smape
-def compute_metrics(eval_pred):
-
-  predictions, references = eval_pred
-  predictions = np.argmax(predictions, axis=1)
-
-  # SMAPE
-  smape = 1/len(references) * np.sum(2 * np.abs(predictions-references) / (np.abs(references) + np.abs(predictions))*100)
- 
-  return {"smape": smape} 
-
 # f1
 def compute_metrics(eval_pred):
 
   predictions, references = eval_pred
-  predictions = np.argmax(predictions, axis=1)
+  predictions = np.argmax(predictions, axis=1) # multi-classification
 
   # 'micro', 'macro', etc. are for multi-label classification. If you are running a binary classification, leave it as default or specify "binary" for average
   return metric.compute(predictions=predictions, references=references, average="micro")  
@@ -21,13 +10,24 @@ def compute_metrics(eval_pred):
 
 # rmse
 from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
 
-# Create Metrics
-def compute_metrics(eval_pred):
+def compute_metrics_for_regression(eval_pred):
+    logits, labels = eval_pred
+    labels = labels.reshape(-1, 1)
 
-  predictions, references = eval_pred
-  predictions = np.argmax(predictions, axis=1)
+    print("Logits:", logits[0:5])
+    print("Labels:", labels[0:5])
+    # print("Labels:", labels)
 
-  rmse = mean_squared_error(references, predictions, squared=False)
+    mse = mean_squared_error(labels, logits)
+    rmse = mean_squared_error(labels, logits, squared=False)
+    mae = mean_absolute_error(labels, logits)
+    r2 = r2_score(labels, logits)
+    smape = 1/len(labels) * np.sum(2 * np.abs(logits-labels) / (np.abs(labels) + np.abs(logits))*100)
+    single_squared_errors = ((logits - labels).flatten()**2).tolist()
+    accuracy = sum([1 for e in single_squared_errors if e < 0.5]) / len(single_squared_errors)
 
-  return {'rmse': rmse}
+    return {"mse": mse, "rmse": rmse, "mae": mae, "smape": smape, "r2": r2, "accuracy": accuracy}

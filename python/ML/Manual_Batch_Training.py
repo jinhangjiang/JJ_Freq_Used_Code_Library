3ad8a92378c5b7a1700c8eb8e93743c47654a072
Jinhang Jiang <60441304+jinhangjiang@users.noreply.github.com>
2022-06-13 10:11:21 -0500

    Create Manual_Batch_Training.py

---
+++ python/ML/Manual_Batch_Training.py
@@ -0,0 +1,13 @@
+batchsize = 100
+temp_df = pd.DataFrame()
+final_df2_len=0
+for i in tqdm_notebook(range(0, len(df), batchsize)):
+    batch = df_final_miss[i:i+batchsize]
+
+    '''
+    process and train the data here
+    
+    output = model.train(batch)
+    '''
+    
+    temp_df = temp_df.append(output)
